35aa2566068fb373230f7fd35e55f43cb3c9df24
Joseph Coffa <josephco440@gmail.com>
2026-02-07 12:32:37 -0500

    Add button to return to main menu in pause screen

--- globals/pause_menu.gd
+++ globals/pause_menu.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+const MAIN_MENU = preload("uid://b4dswwg1tbofl")
+
 @onready var pause_sound: AudioStreamPlayer = $PauseSound
 @onready var unpause_sound: AudioStreamPlayer = $UnpauseSound
 
@@ -31,6 +33,12 @@ func _unhandled_key_input(event: InputEvent) -> void:
 func _on_resume_button_pressed() -> void:
 	hide()
 	get_tree().paused = false
+
+
+func _on_main_menu_button_pressed() -> void:
+	hide()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(MAIN_MENU)
 
 
 func _on_quit_button_pressed() -> void:
